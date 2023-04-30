33877e3f465a44e2372cff8838aa61f41cd68752
SwampWood <danya.ney@yandex.ru>
2023-04-30 23:47:02 +0300

    cats and dogs

---
+++ Бот Случайный котик/random_cat.py
@@ -0,0 +1,33 @@
+import discord
+import logging
+import requests
+
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+TOKEN = "MTEwMjMxOTA3NjY2NTIwNDgzNw.GLWM0Z.gQRcO9FIavjZhm5-SXkJThddB17xDNb7Z7A_4w"
+
+
+class YLBotClient(discord.Client):
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        if "кот" in message.content.lower():
+            response = requests.get('https://api.thecatapi.com/v1/images/search')
+            json_object = response.json()
+            await message.channel.send(json_object[0]['url'])
+        elif "собак" in message.content.lower():
+            response = requests.get('https://dog.ceo/api/breeds/image/random')
+            json_object = response.json()
+            await message.channel.send(json_object['message'])
+
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+client = YLBotClient(intents=intents)
+client.run(TOKEN)
